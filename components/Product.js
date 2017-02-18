4070c154e9dbbb1a41250b35a565a13f65e795ea
NStephenson <nickstephenson90@gmail.com>
2017-02-18 13:16:16 -0500

    done

---
+++ components/Product.js
@@ -0,0 +1,48 @@
+const React = require('react');
+
+class Product extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>
+          {this.props.producer}<br/>
+          {this.props.hasWatermark ? "Watermark" : ""}<br/>
+          {this.props.color}<br/>
+          {this.props.weight}<br/>
+        </p>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  producer: React.PropTypes.string,
+  hasWatermark: React.PropTypes.bool,
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName) => {
+    const weight = props[propName];
+
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required.');
+    }
+
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
+
+    const isValidWeight = weight > 80 && weight < 300;
+
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  },
+
+}
+
+module.exports = Product;
